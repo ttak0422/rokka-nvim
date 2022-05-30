078c0faee637aa0674a01cad8c4f7fc0d26c6511
ttak0422 <ttak0422@gmail.com>
2022-05-31 02:01:02 +0900

    Add pre-commit

--- flake.nix
+++ flake.nix
@@ -1,9 +1,19 @@
 {
   description = "[WIP] Neovim Plugin Manager for Nix.";
 
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-    hmModule = import ./modules/home-manager.nix inputs;
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    {
+      hmModule = import ./modules/home-manager.nix inputs;
+    } // flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ nixfmt luaformatter pre-commit ];
+        };
+      });
 }
