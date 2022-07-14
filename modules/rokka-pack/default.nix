888ef32dcf304e9e9a0909b7a9b162bb5f1ca92f
ttak0422 <ttak0422@gmail.com>
2022-07-14 23:27:49 +0900

    close #18
    remove unused code

--- modules/rokka-pack/default.nix
+++ modules/rokka-pack/default.nix
@@ -52,40 +52,6 @@ let
 
   locatePluginsFt = map locatePluginFt;
 
-  # start/opt directories must be created before this.
-  locateOptimizedPlugin = p:
-    let
-      dir = if p.optional then "opt" else "start";
-      name = p.pname;
-      rtp = if isNull p.rtp then "" else p.rtp;
-      origin = p.plugin;
-      optimized = optimizePackage origin;
-    in ''
-      if [ -e ${origin}/${rtp}/ftdetect ] && [ -n "$(ls ${origin}/${rtp}/ftdetect)" ]; then
-        mkdir -p $out/ftdetect/${name}
-        ln -sf ${origin}/${rtp}/ftdetect/* $out/ftdetect/${name}
-      fi
-
-      if [ -e ${origin}/${rtp}/ftplugin ] && [ -n "$(ls ${origin}/${rtp}/ftplugin)" ]; then
-        mkdir -p $out/ftplugin/${name}
-        ln -sf ${origin}/${rtp}/ftplugin/* $out/ftplugin/${name}
-      fi
-
-      ln -sf ${optimized}/${rtp} $out/pack/${packpath}/${dir}/${name}
-    '';
-
-  # start/opt directories must be created before this.
-  locateNormalPlugin = p:
-    let
-      dir = if p.optional then "opt" else "start";
-      name = p.pname;
-      rtp = if isNull p.rtp then "" else p.rtp;
-      plugin = p.plugin;
-    in "ln -sf ${plugin}/${rtp} $out/pack/${packpath}/${dir}/${name}";
-
-  locatePlugins = map
-    (p: if p.optimize then locateOptimizedPlugin p else locateNormalPlugin p);
-
   start = map (p: p.plugin) (filter (p: !p.optional) allPlugins');
   opt = map (p: p.plugin) (filter (p: p.optional) allPlugins');
 
