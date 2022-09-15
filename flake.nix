6d6c117ce3f9ae0789fa505609a86749a58e0880
ttak0422 <ttak0422@gmail.com>
2022-09-15 23:55:59 +0900

    Add: nixt

--- flake.nix
+++ flake.nix
@@ -5,6 +5,13 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     nix-filter.url = "github:numtide/nix-filter";
+    nixt = {
+      url = "github:nix-community/nixt";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, nix-filter, ... }@inputs:
@@ -12,10 +19,12 @@
       hmModule = import ./modules/home-manager.nix
         (inputs // { nix-filter = nix-filter.lib; });
     } // flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        nixt = inputs.nixt.packages.${system}.nixt;
       in {
         devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nixfmt luaformatter pre-commit ];
+          buildInputs = with pkgs; [ nixfmt luaformatter pre-commit nixt ];
         };
       });
 }
